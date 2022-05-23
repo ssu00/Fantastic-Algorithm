9fb295b5b7de7a15d1cd0bf1da144620b109a5ce
ssu00 <rachel4535@naver.com>
2022-05-23 22:14:30 +0900

    .

---
+++ Lv2/07_12973/ssu00.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    let stack=[];
+    if(s.length%2==1) return 0;
+    for(let i=0;i<s.length;i++){
+        stack[stack.length-1]==s[i] ? stack.pop() : stack.push(s[i]);
+    }
+    return stack.length == 0 ? 1 : 0;
+}
